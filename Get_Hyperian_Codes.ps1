f1f06de2affd4bc355a3a9115eb2f053285afee3
ItsWitchy <kikilovesfoals@gmail.com>
2019-12-12 15:19:07 -0600

    Add files via upload

---
+++ Get_Hyperian_Codes.ps1
@@ -0,0 +1,3 @@
+﻿Get-ADUser -Filter * -Properties OtherMobile,CanonicalName | Select GivenName,Surname,Name,SamAccountName,UserPrincipalName,OtherMobile,CanonicalName,DistinguishedName,Enabled  | Out-GridView
+
+Export-Csv -Path '.\OneDrive - OPRG\Work\Scripts\OtherMobileLookUp.csv' -NoTypeInformation
